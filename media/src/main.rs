108ec543af7a941e054fa502963a65ee6d66c81c
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-01 18:30:48 +0700

    go until option

--- media/src/main.rs
+++ media/src/main.rs
@@ -3,6 +3,8 @@ enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String },
+    Podcast(u32),
+    Placeholder,
 }
 
 impl Media {
@@ -17,6 +19,43 @@ impl Media {
             Media::Audiobook { title } => {
                 format!("Audiobook: {}", title)
             }
+            Media::Podcast(id) => {
+                format!("Podcast: {}", id)
+            }
+            Media::Placeholder => {
+                format!("Placeholder")
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Catalog {
+    items: Vec<Media>,
+}
+
+impl Catalog {
+    fn new() -> Self {
+        Catalog { items: Vec::new() }
+    }
+
+    fn add(&mut self, media: Media) {
+        self.items.push(media);
+    }
+
+    fn describe(&self) -> String {
+        self.items
+            .iter()
+            .map(|m| m.description())
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    fn get_by_index(&self, index: usize) -> Option<&Media> {
+        if self.items.len() > index {
+            Some(&self.items[index])
+        } else {
+            None
         }
     }
 }
@@ -25,18 +64,35 @@ fn main() {
     let audiobook = Media::Audiobook {
         title: "An Audiobook".to_string(),
     };
-
     let good_movie = Media::Movie {
         title: String::from("Good Movie"),
         director: String::from("Good Director"),
     };
-
     let bad_book = Media::Book {
         title: "Bad Book".to_string(),
         author: "Bad Author".to_string(),
     };
+    let podcast = Media::Podcast(42);
+    let placeholder = Media::Placeholder;
 
-    println!("{}", audiobook.description());
-    println!("{}", good_movie.description());
-    println!("{}", bad_book.description());
+    let mut catalog = Catalog::new();
+
+    catalog.add(audiobook);
+    catalog.add(good_movie);
+    catalog.add(bad_book);
+    catalog.add(podcast);
+    catalog.add(placeholder);
+
+    println!("Catalog: \n{}", catalog.describe());
+    println!("-------");
+
+    // Item: Audiobook { title: "An Audiobook" }
+    match catalog.get_by_index(0) {
+        Some(value) => {
+            println!("Item: {:?}", value);
+        }
+        None => {
+            println!("No value available");
+        }
+    }
 }
