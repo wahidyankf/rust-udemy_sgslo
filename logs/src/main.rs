e756e8d05dc19c1e969ffac3ddad2f14801d6fc2
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-03 06:45:37 +0700

    play with strings

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,7 +1,16 @@
 use std::fs;
 
-fn main() {
-    let text = fs::read_to_string("logs.txt");
+fn string_test(a: String, b: &String, c: &str) {}
 
-    println!("{:?}", text);
+fn main() {
+    match fs::read_to_string("logs.txt") {
+        Ok(text_that_was_read) => {
+            println!("{}", text_that_was_read)
+        }
+        Err(why_this_failed) => {
+            println!("{}", why_this_failed)
+        }
+    }
+
+    string_test(String::from("red"), &String::from("red"), "red")
 }
