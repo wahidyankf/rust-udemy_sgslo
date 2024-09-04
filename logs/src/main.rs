b432926545f20823758ffe1c0c9cb880c14c124a
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-04 07:10:11 +0700

    finish section 7

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,16 +1,48 @@
 use std::fs;
+use std::io::Error;
 
-fn string_test(a: String, b: &String, c: &str) {}
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
 
-fn main() {
-    match fs::read_to_string("logs.txt") {
-        Ok(text_that_was_read) => {
-            println!("{}", text_that_was_read)
-        }
-        Err(why_this_failed) => {
-            println!("{}", why_this_failed)
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
         }
     }
 
-    string_test(String::from("red"), &String::from("red"), "red")
+    results
+}
+
+fn main() -> Result<(), Error> {
+    let text = fs::read_to_string("logs.txt")?;
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n"))?;
+    fs::write("./test/error.txt", error_logs.join("\n")).expect("failed to write test");
+
+    Ok(())
+
+    // let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+    //
+    // let error_logs = extract_errors(text.as_str());
+    // fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
+
+    // // match fs::read_to_string("logs.txt") {
+    //     Ok(text_that_was_read) => {
+    //         let error_logs = extract_errors(text_that_was_read.as_str());
+    //
+    //         match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(..) => {
+    //                 println!("Wrote errors.txt")
+    //             }
+    //             Err(reason_write_failed) => {
+    //                 println!("Writing of errors.txt failed: {}", reason_write_failed)
+    //             }
+    //         }
+    //     }
+    //     Err(why_this_failed) => {
+    //         println!("{}", why_this_failed)
+    //     }
+    // }
 }
