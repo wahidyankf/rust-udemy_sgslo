1d05009435d9881eb003c1dd85cfc899161158c9
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-05 06:38:04 +0700

    go until iter adaptor

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,3 +1,43 @@
+fn print_elements_for(elements: &Vec<String>) {
+    for element in elements.iter() {
+        println!("{}", element);
+    }
+}
+
+fn print_elements_for_each(elements: &Vec<String>) {
+    elements.iter().for_each(|element| {
+        println!("{}", element);
+    });
+}
+
+fn print_elements_iter_adaptor(elements: &Vec<String>) {
+    elements
+        .iter()
+        .map(|el| format!("{} {}", el, el))
+        .for_each(|el| println!("{}", el));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let colors = vec![
+        String::from("red"),
+        String::from("green"),
+        String::from("blue"),
+    ];
+
+    let mut colors_iter = colors.iter();
+
+    println!("colors: {:?}", colors_iter);
+    println!("colors: {:?}", colors_iter.next());
+    println!("colors: {:?}", colors_iter.next());
+    println!("colors: {:?}", colors_iter.next());
+    println!("colors: {:?}", colors_iter.next());
+
+    println!("---");
+
+    print_elements_for(&colors);
+    println!("---");
+    print_elements_for_each(&colors);
+    println!("---");
+    print_elements_iter_adaptor(&colors);
+    println!("---");
 }
