928ca965f88badacdc6a36b3452fd1d946ccc5a3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-06 06:19:54 +0700

    go until collect

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -23,6 +23,14 @@ fn print_elements_vec_slice(elements: &[String]) {
     }
 }
 
+fn shorten_strings(elements: &mut [String]) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
+
+fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements.iter().map(|el| el.to_uppercase()).collect()
+}
+
 fn main() {
     let colors = vec![
         String::from("red"),
@@ -30,8 +38,9 @@ fn main() {
         String::from("blue"),
     ];
 
-    let mut colors_iter = colors.iter();
+    let mut colors_mut = colors.clone();
 
+    let mut colors_iter = colors.iter();
     println!("colors: {:?}", colors_iter);
     println!("colors: {:?}", colors_iter.next());
     println!("colors: {:?}", colors_iter.next());
@@ -47,5 +56,17 @@ fn main() {
     print_elements_iter_adaptor(&colors);
     println!("---");
     print_elements_vec_slice(&colors[1..3]);
+    println!("---");
+
+    shorten_strings(&mut colors_mut);
+    println!("{:?}", colors);
+    println!("---");
+
+    shorten_strings(&mut colors_mut[1..3]);
+    println!("{:?}", colors);
+    println!("---");
+
+    let uppercased = to_uppercase(&colors);
+    println!("{:?}", uppercased);
     println!("---");
 }
