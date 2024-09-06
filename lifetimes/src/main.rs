96fe153a3140c641aeed6a9ac28410d0d008368b
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-07 06:39:11 +0700

    go until lifetime

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,45 @@
+fn next_language<'a>(languages: &'a [String], current: &str) -> &'a str {
+    let mut found = false;
+
+    for lang in languages {
+        if found {
+            return lang;
+        }
+
+        if lang == current {
+            found = true;
+        }
+    }
+
+    languages.last().unwrap()
+}
+
+fn last_language(languages: &[String]) -> &str {
+    languages.last().unwrap()
+}
+
+fn longest_language<'a>(lang_a: &'a str, lang_b: &'a str) -> &'a str {
+    if lang_a.len() > lang_b.len() {
+        lang_a
+    } else {
+        lang_b
+    }
+}
+
+fn main() {
+    let languages = vec![
+        String::from("rust"),
+        String::from("go"),
+        String::from("typescript"),
+        String::from("javascript"),
+    ];
+
+    let result_next = next_language(&languages, "go");
+    println!("{}", result_next);
+
+    let result_last = last_language(&languages);
+    println!("{}", result_last);
+
+    let result_longest = longest_language("rust", "go");
+    println!("{}", result_longest);
+}
